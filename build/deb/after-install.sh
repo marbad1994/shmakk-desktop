4bf4dc92da5351dffe504e12ad0f51489041d8d0
marbad1994 <marcus.e.r.bader@gmail.com>
2026-05-24 07:46:05 +0300

    nu så

---
+++ build/deb/after-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if type update-alternatives 2>/dev/null >&1; then
+    # Remove previous link if it doesn't use update-alternatives
+    if [ -L '/usr/bin/${executable}' -a -e '/usr/bin/${executable}' -a "`readlink '/usr/bin/${executable}'`" != '/etc/alternatives/${executable}' ]; then
+        rm -f '/usr/bin/${executable}'
+    fi
+    update-alternatives --install '/usr/bin/${executable}' '${executable}' '/opt/${sanitizedProductName}/${executable}' 100 || ln -sf '/opt/${sanitizedProductName}/${executable}' '/usr/bin/${executable}'
+else
+    ln -sf '/opt/${sanitizedProductName}/${executable}' '/usr/bin/${executable}'
+fi
+
+cat > '/usr/bin/shmakk' <<'EOF'
+#!/bin/sh
+export SHMAKK_CLI=1
+exec '/opt/${sanitizedProductName}/${executable}' "$@"
+EOF
+chmod 0755 '/usr/bin/shmakk'
+
+# SUID chrome-sandbox for Electron 5+
+chmod 4755 '/opt/${sanitizedProductName}/chrome-sandbox' || true
+
+if hash update-mime-database 2>/dev/null; then
+    update-mime-database /usr/share/mime || true
+fi
+
+if hash update-desktop-database 2>/dev/null; then
+    update-desktop-database /usr/share/applications || true
+fi
